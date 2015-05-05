bd83b552302bcb2afc9ad33ab54a38c987e31f76
Desmond Lee <desmondclee@gmail.com>
2015-05-04 22:06:11 -0500

    build

--- sign.sh
+++ sign.sh
@@ -1,5 +1,5 @@
 #keytool -genkey -v -keystore keys/release-key.keystore -alias deskey -keyalg RSA -keysize 2048 -validity 10000
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../keys/release-key.keystore $1 deskey
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../keys/release-key.keystore platforms/android/ant-build/MainActivity-release-unsigned.apk deskey
 
-/Users/desmond/AppData/Local/Android/sdk/build-tools/21.1.2/zipalign.exe -v 4 $1 ./Journey-signed.apk
+C:/Users/desmond/AppData/Local/Android/sdk/build-tools/21.1.2/zipalign.exe -v 4 platforms\android\ant-build\MainActivity-release-unsigned.apk ./Journey-signed.apk
